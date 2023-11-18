c61b36834d182da5d54fad6d87a5ae876397a308
ampelme <olajohnfilhmar@gmail.com>
2023-11-18 18:42:12 +0800

    navigationbar

--- components/src/components/Home.js
+++ components/src/components/Home.js
@@ -1,9 +1,26 @@
+import Link from 'react-router-dom/cjs/react-router-dom.min';
+import Navbar from 'flowbite-react';
+
 const Home = () => {
     return (
         <>
-            <div className="home container mx-auto bg-grey-200">
-                <h1>hello im home</h1>
-            </div>
+            <Navbar fluid rounded>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+                    <Navbar.Link as={Link} to="/canubingmain" active>
+                        CanubingMain
+                    </Navbar.Link>
+                    <Navbar.Link as={Link} to="/canubing1_2">
+                        Canubing1_2
+                    </Navbar.Link>
+                    <Navbar.Link as={Link} to="/bayanan">
+                        Bayanan
+                    </Navbar.Link>
+                    <Navbar.Link as={Link} to="/malinao">
+                        Malinao
+                    </Navbar.Link>
+            </Navbar.Collapse>
+            </Navbar>
         </>
     );
 }
