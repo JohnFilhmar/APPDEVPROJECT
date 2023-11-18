1bb03544ade2da81abf5846cddc422d3320250ce
johnadriellelalongisip <144537154+johnadriellelalongisip@users.noreply.github.com>
2023-11-18 21:57:32 +0800

    hooyeah
    hehehe

--- components/src/components/Canubing1_2.js
+++ components/src/components/Canubing1_2.js
@@ -1,9 +1,56 @@
+
 const Branch = () => {
     return (
         <>
-            <div className="container mx-auto">
-                
+            <div class="md:container md:mx-auto max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-bold">About Us – JoeMar Motorshop</div>
+
+            <p class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">Welcome to <span>JoeMar Motorshop,</span> where our 
+                commitment to quality and passion for motors 
+                drive us to be your ultimate destination for
+                 premium motor parts. Established by [Owner's Name]
+                on [Establishment Date], our multi-branched motor
+                shop is designed to meet the
+                diverse needs of automotive enthusiasts.</p> 
+
+                <div class="uppercase tracking-wide text-sm text-indigo-500 font-bold">Canubing I Main Branch</div>    
+                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">	Establishment Date: [Date]</div>
+<br></br>
+<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Address: [123 Main Street, Downtown City, State, Zip]</div>
+<p class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">At Canubing I Main Branch, 
+we bring the urban motor experience to life. 
+<br></br>
+Founded by [Owner's Name] on [Date], our branch in the 
+heart of the city is
+<br></br> dedicated to offering a premium 
+selection of motor parts for various leading brands.
+</p> 
+<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Our Story</div>
+<p class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+Established in [Year], JoeMar Motorshop was born out of a shared love for automobiles
+<br></br> and a commitment to delivering premium motor parts to fellow enthusiasts. 
+<br></br>Our journey started with a simple idea: to create a one-stop-shop where customers could find everything
+ they need to enhance, repair, or maintain their vehicles.<br></br>
+Over the years, we have grown into a trusted online marketplace, serving customers nationwide.
+<br></br> Our success is built on a foundation of customer satisfaction, quality products, and a passion for all things automotive.
+</p>
+<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">What Sets Us Apart?</div>
+<p class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+At [Your Shop Name], we take pride in offering a curated selection of motor parts from reputable brands, ensuring that our customers receive products that meet or exceed industry standards. Here's what sets us apart:<br></br>
+1.	Quality Assurance: We understand the importance of reliable performance. That's why we source our products from trusted manufacturers to guarantee durability and optimal functionality.<br></br>
+2.	Wide Range of Products: Whether you're looking for replacement parts, performance upgrades, or accessories, we have you covered. Explore our extensive catalog to find the perfect fit for your vehicle.<br></br>
+3.	User-Friendly Online Experience: Our website is designed with you in mind. Enjoy a hassle-free browsing experience, easy navigation, and secure checkout. We want your online shopping journey to be as smooth as possible.<br></br>
+4.	Expert Customer Support: Have a question or need assistance? Our knowledgeable and friendly customer support team is here to help. We are dedicated to providing timely and helpful responses to ensure your satisfaction.<br></br></p>
+<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Our Commitment to Sustainability</div>
+<p class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl"><span>At JoeMar Motorshop ,</span>we are committed to reducing our environmental footprint. We strive to work with eco-friendly manufacturers 
+<br></br>and implement sustainable practices in our operations. By choosing us, you're not only getting quality motor parts but also 
+<br></br>supporting a commitment to a greener future.</p>
+<div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Connect with Us</div>
+<p class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">Join our community of motor enthusiasts on social media. Follow us on Facebook to stay updated on the latest products, promotions, and automotive trends. We love hearing from our customers and seeing how our products enhance your driving experience.
+Thank you for choosing JoeMar Motorshop. We look forward to being your trusted partner in all your motor part needs. Drive with confidence!</p>
             </div>
+         
         </>
     );
 }
--- components/tailwind.config.js
+++ components/tailwind.config.js
@@ -1,14 +1,59 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+     
+        gradient: 'linear-gradient(to bottom, #f2f2f2, #d9d9d9, #fff)',
+      
+     
+      container: {
+        padding: {
+          DEFAULT: '20px',
+        },
+        margin: {
+          DEFAULT: '0 auto',
+        },
+        width: {
+          DEFAULT: '500px',
+        },
+        border: {
+          DEFAULT: '1px solid #ccc',
+        },
+        borderRadius: {
+          DEFAULT: '5px',
+        },
+        backgroundColor: {
+          DEFAULT: '#fff',
+        },
+        boxShadow: {
+          DEFAULT: '0 0 10px rgba(0, 0, 0, 0.2)',
+        },
+        textAlign: {
+          DEFAULT: 'center',
+        },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+     
+    },
+
   },
   plugins: [
     require('flowbite/plugin')
   ],
-}
+  backgroundImage: {
+    gradient: 'linear-gradient(to bottom, #f2f2f2, #d9d9d9, #fff)',
+  },
+  
+    }
 
+  }
